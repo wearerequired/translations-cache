8f815aaf069cc5df684df325a00a3e464427b03d
Dominik Schilling <dominikschilling+git@gmail.com>
2023-01-11 09:45:34 +0100

    Spread expires over a wider window to prevent high read requests in a single request

--- translations-cache.php
+++ translations-cache.php
@@ -64,7 +64,7 @@ function load_script_translations( $translations, $file, string $handle, string 
 	$cache_key = 'load_script_translations:' . md5( $cache_key_salt . $locale . serialize( \func_get_args() ) );
 
 	$found        = false;
-	$translations = apcu_fetch( $cache_key, $found );
+	$translations = cache_fetch( $cache_key, $found );
 	if ( ! $found ) {
 		// Call the core function to load the translations without the pre-filter.
 		remove_filter( 'pre_load_script_translations', __NAMESPACE__ . '\load_script_translations', 9999 );
@@ -72,12 +72,7 @@ function load_script_translations( $translations, $file, string $handle, string 
 		add_filter( 'pre_load_script_translations', __NAMESPACE__ . '\load_script_translations', 9999, 4 );
 
 		// Cache the result.
-		if ( false === $translations ) {
-			$expiration = HOUR_IN_SECONDS;
-		} else {
-			$expiration = DEFAULT_EXPIRE;
-		}
-		apcu_add( $cache_key, $translations, $expiration );
+		cache_add( $cache_key, $translations, false === $translations ? HOUR_IN_SECONDS : DEFAULT_EXPIRE );
 	}
 
 	return $translations;
@@ -108,13 +103,13 @@ function load_textdomain( bool $override, string $domain, string $mofile ): bool
 	$cache_key = 'load_textdomain:' . md5( $cache_key_salt . $locale . serialize( \func_get_args() ) );
 
 	$found = false;
-	$data  = apcu_fetch( $cache_key, $found );
+	$data  = cache_fetch( $cache_key, $found );
 	if ( ! $found ) {
 		// Allow plugins to change the .mo file.
 		$mofile = apply_filters( 'load_textdomain_mofile', $mofile, $domain ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals -- Core filter.
 
 		if ( ! is_readable( $mofile ) ) {
-			apcu_add( $cache_key, false, HOUR_IN_SECONDS );
+			cache_add( $cache_key, $data, HOUR_IN_SECONDS );
 
 			// Return true since we still override the .mo file loading.
 			return true;
@@ -124,7 +119,7 @@ function load_textdomain( bool $override, string $domain, string $mofile ): bool
 		if ( ! $mo->import_from_file( $mofile ) ) {
 			$wp_textdomain_registry->set( $domain, $locale, false );
 
-			apcu_add( $cache_key, false, HOUR_IN_SECONDS );
+			cache_add( $cache_key, $data, HOUR_IN_SECONDS );
 
 			// Return true since we still override the .mo file loading.
 			return true;
@@ -137,7 +132,7 @@ function load_textdomain( bool $override, string $domain, string $mofile ): bool
 			'entries' => $mo->entries,
 			'headers' => $mo->headers,
 		];
-		apcu_add( $cache_key, $data, DEFAULT_EXPIRE );
+		cache_add( $cache_key, $data, DEFAULT_EXPIRE );
 
 		if ( isset( $l10n[ $domain ] ) ) {
 			$mo->merge_with( $l10n[ $domain ] );
@@ -160,3 +155,33 @@ function load_textdomain( bool $override, string $domain, string $mofile ): bool
 	return true;
 }
 add_filter( 'override_load_textdomain', __NAMESPACE__ . '\load_textdomain', 9999, 4 );
+
+/**
+ * Caches data to APCu, only if it's not already stored.
+ *
+ * @param string $key    The cache key to use for retrieval later.
+ * @param mixed  $data   The contents to store in the cache.
+ * @param int    $expire Optional. When to expire the cache contents, in seconds.
+ *                       Default 0 (no expiration).
+ * @return bool|array Returns true if something has effectively been added into the cache,
+ *                    false otherwise. Second syntax returns array with error keys.
+ */
+function cache_add( string $key, $data, int $expire = 0 ) {
+	// Alter provided expire values to be within -10%/+20% of the provided time,
+	// to spread expires over a wider window.
+	$expire = rand( \intval( $expire * 0.9 ), \intval( $expire * 1.2 ) ); // phpcs:ignore WordPress.WP.AlternativeFunctions.rand_rand
+
+	return apcu_add( $key, $data, $expire );
+}
+
+/**
+ * Retrieves the cache contents from APCu by key.
+ *
+ * @param string    $key   The key under which the cache contents are stored.
+ * @param bool|null $found Optional. Whether the key was found in the cache (passed by reference).
+ *                         Disambiguates a return of false, a storable value. Default null.
+ * @return mixed The stored variable or array of variables on success; false on failure.
+ */
+function cache_fetch( string $key, ?bool &$found = null ) {
+	return apcu_fetch( $key, $found );
+}
